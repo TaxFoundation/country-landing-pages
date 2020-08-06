407988de80c905523672ff68ebeae4f40b99cfbc
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-08-06 16:15:31 -0400

    Remove hashes from generated files

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-csv`,
+    `gatsby-plugin-remove-fingerprints`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
