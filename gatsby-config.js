9dcd5c7aa25a850e6e136922df69fb07f3b330cd
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-08-10 11:15:16 -0400

    Fix stupid typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    ,
   ],
   assetPrefix:
     // 'http://localhost/wp-content/themes/tf-wp-theme/templates/country-landing-pages',
