0c2d570dc196fb90e6d71782d0e7ed2e1208a959
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-08-11 10:56:41 -0400

    Yet cleaner config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -12,8 +10,5 @@ module.exports = {
     },
     `gatsby-transformer-csv`,
     `gatsby-plugin-styled-components`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
