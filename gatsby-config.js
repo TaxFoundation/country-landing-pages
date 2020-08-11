e8c459461de766db73c14d877829a2b3d0125c49
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-08-11 10:52:39 -0400

    Clean up gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,6 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -19,8 +12,6 @@ module.exports = {
     },
     `gatsby-transformer-csv`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-remove-fingerprints`,
-    'gatsby-plugin-no-sourcemaps',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
