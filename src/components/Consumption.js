9984842a097d6d4f47ecd045a88e8055cd62b1e6
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-08-31 11:34:28 -0400

    Fixes #33

--- src/components/Consumption.js
+++ src/components/Consumption.js
@@ -4,6 +4,7 @@ import { graphql } from 'gatsby';
 
 import Wrapper from './ui/Wrapper';
 import { ChartTabs, ChartTab } from './ui/ChartTabs';
+import { KeyFigures, KeyFigure } from './ui/KeyFigures';
 import VATRatesChart from './charts/VATRatesChart';
 import VATBaseChart from './charts/VATBaseChart';
 
@@ -11,16 +12,16 @@ const Consumption = ({ data }) => {
   const [activeTab, setActiveTab] = useState('vat-rates');
   let country = { ...data.countriesCsv, data: {} };
   const itciMaxYear = Math.max(
-    ...data.allIndexRawDataCsv.edges.map(edge => +edge.node.year)
+    ...data.allIndexRawDataCsv.nodes.map(node => +node.year)
   );
-  const theData = data.allIndexRawDataCsv.edges
-    .filter(edge => +edge.node.year === itciMaxYear)
-    .map(edge => {
+  const theData = data.allIndexRawDataCsv.nodes
+    .filter(node => +node.year === itciMaxYear)
+    .map(node => {
       return {
-        iso3: edge.node.ISO_3,
-        vatBreadth: +edge.node.vat_base,
-        vatRate: +edge.node.vat_rate,
-        vatThreshold: +edge.node.vat_threshold,
+        iso3: node.ISO_3,
+        vatBreadth: +node.vat_base,
+        vatRate: +node.vat_rate,
+        vatThreshold: +node.vat_threshold,
       };
     });
   const tabOptionsFunc = countryId => [
@@ -86,6 +87,26 @@ const Consumption = ({ data }) => {
         </a>
         .
       </p>
+      <KeyFigures>
+        <KeyFigure>
+          <h3>Share of Revenue from Consumption Taxes</h3>
+          <div>{`${data.sourceRevenueByCountryCsv.Consumption_Taxes}%`}</div>
+        </KeyFigure>
+        <KeyFigure>
+          <h3>Average VAT Rate</h3>
+          <div>{`${
+            data.allIndexRawDataCsv.nodes.find(d => d.ISO_3 === country.iso3)
+              .vat_rate
+          }%`}</div>
+        </KeyFigure>
+        <KeyFigure>
+          <h3>Average Business Compliance Time in Hours</h3>
+          <div>{`${
+            data.allIndexRawDataCsv.nodes.find(d => d.ISO_3 === country.iso3)
+              .consumption_time
+          }`}</div>
+        </KeyFigure>
+      </KeyFigures>
     </Wrapper>
   );
 };
@@ -100,53 +121,17 @@ export const query = graphql`
       article
     }
     allIndexRawDataCsv {
-      edges {
-        node {
-          ISO_3
-          year
-          asset_tax
-          buildings_cost_recovery
-          capital_duties
-          capital_gains_exemption
-          capital_gains_rate
-          cfc_rules
-          consumption_time
-          corporate_rate
-          corporate_time
-          country_limitations
-          dividends_exemption
-          dividends_rate
-          dividends_withholding_tax
-          estate_or_inheritance_tax
-          financial_transaction_tax
-          index_capital_gains
-          intangibles_cost_recovery
-          interest_withholding_tax
-          inventory
-          labor_payments
-          labor_time
-          loss_carryback
-          loss_carryforward
-          machines_cost_recovery
-          net_wealth
-          other_payments
-          patent_box
-          profit_payments
-          property_tax
-          property_tax_collections
-          r_and_d_credit
-          royalties_withholding_tax
-          tax_treaties
-          tax_wedge
-          thin_capitalization_rules
-          threshold_top_income_rate
-          top_income_rate
-          transfer_tax
-          vat_base
-          vat_rate
-          vat_threshold
-        }
+      nodes {
+        ISO_3
+        year
+        consumption_time
+        vat_base
+        vat_rate
+        vat_threshold
       }
+    }
+    sourceRevenueByCountryCsv(iso_3: { eq: $iso3 }) {
+      Consumption_Taxes
     }
   }
 `;
